3c1c5bd4c896c4fd6a05bc7087f5c42604761e45
Daan Zwarthoed <78368488+DaanZwarthoed@users.noreply.github.com>
2021-04-21 11:26:53 +0200

    added a few pages

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.static('static/public'));
 
 app.get('/', (req, res) => {
     res.sendFile('./static/public/index.html', { root: __dirname });
 })
+
+app.get('/about', (req, res) => {
+  res.sendFile('./static/public/about.html', { root: __dirname });
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile('./static/public/login.html', { root: __dirname });
+})
+
+// app.get('*', function(req, res){
+//   res.send('404 page not found', 404);
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
