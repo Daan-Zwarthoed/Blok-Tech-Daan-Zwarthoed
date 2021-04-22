708c0ccd980c5077427cfeaa3cd68748be45ae5d
Daan Zwarthoed <78368488+DaanZwarthoed@users.noreply.github.com>
2021-04-22 16:56:58 +0200

    added filtering on games

--- server.js
+++ server.js
@@ -6,15 +6,32 @@ app.set('views', './static/public')
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index', {route: 'pages/profiel/profiel.pug'});
+  res.render('index', {route: 'pages/profiel/profiel.pug', title: 'Profiel'});
+})
+
+app.get('/filteren', (req, res) => {
+  res.render('index', {route: 'pages/profiel/profiel.pug', title: 'Filteren', filterStap: 'KiesSpel'});
+})
+
+app.get('/filteren/rocketleague', (req, res) => {
+  res.render('index', {route: 'pages/profiel/profiel.pug', title: 'Filteren', filterStap: 'Rocket League'});
+})
+app.get('/filteren/schaken', (req, res) => {
+  res.render('index', {route: 'pages/profiel/profiel.pug', title: 'Filteren', filterStap: 'Schaken'});
+})
+app.get('/filteren/apexlegends', (req, res) => {
+  res.render('index', {route: 'pages/profiel/profiel.pug', title: 'Filteren', filterStap: 'Apex Legends'});
+})
+app.get('/filteren/modernwarfare', (req, res) => {
+  res.render('index', {route: 'pages/profiel/profiel.pug', title: 'Filteren', filterStap: 'Modern Warfare'});
 })
 
 app.get('/zoeken', (req, res) => {
-  res.render('index', {route: 'pages/zoeken/zoeken.pug'});
+  res.render('index', {route: 'pages/zoeken/zoeken.pug', title: 'Zoeken'});
 })
 
 app.get('/berichten', (req, res) => {
-  res.render('index', {route: 'pages/berichten/berichten.pug'});
+  res.render('index', {route: 'pages/berichten/berichten.pug', title: 'Berichten'});
 })
 
 app.get('*', function(req, res){
