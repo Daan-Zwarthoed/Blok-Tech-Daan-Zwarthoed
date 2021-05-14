cf0fe63b95d8d6d14e0a0b8db594ff7d4674bab2
Daan Zwarthoed <78368488+DaanZwarthoed@users.noreply.github.com>
2021-05-14 13:43:45 +0200

    added process.env.PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 
 app.set('views', './static/public');
