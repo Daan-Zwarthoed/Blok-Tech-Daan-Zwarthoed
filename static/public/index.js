8375d867790a3faa71fb1adb5f87896f9c15a09c
Daan Zwarthoed <78368488+DaanZwarthoed@users.noreply.github.com>
2021-05-18 15:30:23 +0200

    move noggeenfilter on scroll and refactor

--- static/public/index.js
+++ static/public/index.js
@@ -8,3 +8,17 @@ function makeRankRequired() {
 if (rankedCheckbox) {
   rankedCheckbox.addEventListener("change", makeRankRequired);
 }
+
+let nogGeenFilters = document.getElementsByClassName("nogGeenFilters")[0];
+
+function toggleNogGeenFiltersScroll() {
+  if (window.scrollY > 300) {
+    nogGeenFilters.classList.add("nogGeenFiltersScroll");
+  } else {
+    nogGeenFilters.classList.remove("nogGeenFiltersScroll");
+  }
+}
+
+if (nogGeenFilters) {
+  window.addEventListener("scroll", toggleNogGeenFiltersScroll);
+}
